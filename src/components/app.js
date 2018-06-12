0787bcce175d5e59cc9f08035ece829f701c38e4
selms <selmsb@gmail.com>
2018-06-12 15:52:27 -0600

    change port add title

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,8 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>DevCamp React Starter</h1>
+        <h1>Bottega madlibs</h1>
+
       </div>
     );
   }
