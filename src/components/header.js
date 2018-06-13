b29e4d8e22881dcfde067fb327163bb7827d7f3a
selms <selmsb@gmail.com>
2018-06-12 22:57:23 -0600

    madlib

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = (title, content) => {
+    return (
+        <div className="header">
+             <hi>(titel)</h1>
+             <p>(content)</p>
+        </div>
+    )
+}
+
+export default Header;
