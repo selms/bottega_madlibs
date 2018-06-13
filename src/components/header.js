96d194dc2d6c9ae034369d02f42e65b1f9bd0b6b
selms <selmsb@gmail.com>
2018-06-13 10:04:45 -0600

    built our first class componet for the inputs container, and built an input functional component to render custome inpouts

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Header = (title, content) => {
     return (
         <div className="header">
-             <hi>(titel)</h1>
-             <p>(content)</p>
+             <h1>(Bottega Madlibs)</h1>
+             <p>('Fill out the fields below and click the generate button to see the Mad Lib story.' )</p>
         </div>
     )
 }
