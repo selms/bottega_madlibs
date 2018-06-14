4434f5ca06fbd27a32c1fd569922f4a9d648dd6f
selms <selmsb@gmail.com>
2018-06-14 10:56:27 -0600

    added remaining inputs

--- src/components/input.js
+++ src/components/input.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Input = (title, state, onChange, name) => {
+const Input = ({title, state, name}, onChange) => {
     return (
-        <div className="input">
+        <div key={name} className="input">
             <input name={name} value={state} onChange={onChange}/>
             <label>{title}</label>
         </div>
     )
 }
 
-export default Input;
+export default Input
